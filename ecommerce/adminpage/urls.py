441bc774d48e4d816d0ad02a87401ef945f5b189
AyushHimalayaShrestha <aayush625@gmail.com>
2025-08-25 21:48:17 +0545

    add product template

--- ecommerce/adminpage/urls.py
+++ ecommerce/adminpage/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('category/add',views.add_category,name='add_category'),
-    path('product/add',views.add_category,name='add_product'),
+    path('product/add',views.add_product,name='add_product'),
 
 ]
